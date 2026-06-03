b3fa3f266b1c6fab004388ef79aecc507aaaf4b2
Sebastian Rios <juanriosubeda@gmail.com>
2026-06-02 19:45:18 -0700

    Development enviroment setup complete

--- 01-types-and-variables/Hello World/Hello World/main.cpp
+++ 01-types-and-variables/Hello World/Hello World/main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!";
+	
+	cout << "Hello World! \n"
+		 << "My name is Sebastian \n"
+		 << "And this is C++";
+	
+	cout << endl;
+
+	cout << 42 << " is a number\n"
+		 << "Another number is " << 3.14;
 }
