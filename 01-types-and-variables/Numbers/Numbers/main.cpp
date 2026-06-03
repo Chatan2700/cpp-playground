9d57ed4d6757a232736e678cb2de3d2086ddec85
Sebastian Rios <juanriosubeda@gmail.com>
2026-06-03 09:47:16 -0700

    Nums overview

---
+++ 01-types-and-variables/Numbers/Numbers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Numberzzzz\n";
+	float armor{ 0.2 };
+	int largerNumber{ 100'000'000 }; // Thousand Separators 
+
+	// Level is initialized with a value of 5
+	int level{ 2 + 3 };
+	
+	level = 5 * 2; // Updated with the value of 10
+
+	// Some other Examples
+	level = 5 + 1; //6
+	level = 5 - 1; //4
+	level = 5 * 2; //10
+	level = 6 / 2; //3
+	level = 1 + 2 + 3; //6
+
+	//
+
+}
